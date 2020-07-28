646e61881ce4cff6357f4079f129d9b10ef51d63
caiyongqi <1592341907@qq.com>
2020-07-28 14:26:33 +0800

    ''

--- src/main/java/com/graduation/practice/controller/UserController.java
+++ src/main/java/com/graduation/practice/controller/UserController.java
@@ -10,19 +10,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @RequestMapping("/user")
 @Controller
 public class UserController {
     private final UserService userService;
-
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @GetMapping("/")
     public String index(){
-        return "/login.html";
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home(){
+        return "home";
     }
 
     @PostMapping("/login")
@@ -41,9 +47,25 @@ public class UserController {
         }else if(user.getPassword().equals(password)){
             result.setMessage("登录成功");
             result.setData(user);
+            // 存入session中
+            request.getSession().setAttribute("user", user);
         }else{
             result.setMessage("密码错误");
         }
         return result;
     }
+
+    @PostMapping("/findAllAdmin")
+    @ResponseBody
+    public Result findAllAdmin(HttpServletRequest request, HttpSession session){
+        // 获取参数
+        User user = (User) session.getAttribute("user");
+        // 查询
+        List<User> users = userService.findAllAdmin(user);
+        // 结果对象
+        Result<User> result = new Result<>();
+
+        return result;
+    }
+
 }
--- src/main/java/com/graduation/practice/service/UserService.java
+++ src/main/java/com/graduation/practice/service/UserService.java
@@ -2,6 +2,9 @@ package com.graduation.practice.service;
 
 import com.graduation.practice.entity.User;
 
+import java.util.List;
+
 public interface UserService {
     User findUserByAccount(User user);
+    List<User> findAllAdmin(User user);
 }
