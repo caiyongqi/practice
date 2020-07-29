e3b4b63cdfce0ee1a8a9ee330be728552b1e44ff
icepaper <1641546473@qq.com>
2020-07-29 23:53:13 +0800

    merge

--- src/main/java/com/graduation/practice/controller/UserController.java
+++ src/main/java/com/graduation/practice/controller/UserController.java
@@ -4,10 +4,8 @@ import com.graduation.practice.entity.Result;
 import com.graduation.practice.entity.User;
 import com.graduation.practice.service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
