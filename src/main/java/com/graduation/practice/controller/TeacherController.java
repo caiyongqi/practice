93102d0bc9f0e81f38687d047bccbbab3b03a125
caiyongqi <1592341907@qq.com>
2020-08-01 13:46:34 +0800

    '成绩模块相关界面'

--- src/main/java/com/graduation/practice/controller/TeacherController.java
+++ src/main/java/com/graduation/practice/controller/TeacherController.java
@@ -11,20 +11,20 @@ import com.graduation.practice.service.TeacherService;
 import com.graduation.practice.service.UserService;
 import com.graduation.practice.utils.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.JsonbHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @RequestMapping("/teacher")
 @Controller
@@ -38,7 +38,11 @@ public class TeacherController {
         this.userService = userService;
         this.collegeService = collegeService;
     }
-
+    /**
+     *
+     * 管理员操作部分
+     *
+     * **/
     @GetMapping("/findAllTeacher")
     public ModelAndView findAllTeacher(@RequestParam(defaultValue = "1") int pageNum, @RequestParam(defaultValue = "10") int pageSize, HttpSession session){
 
@@ -112,7 +116,6 @@ public class TeacherController {
         String teacherId = request.getParameter("teacherId");
         int pageNum = Integer.parseInt(request.getParameter("pageNum"));
         Teacher teacher = teacherService.findTeacherByTeacherId(new Teacher(teacherId));
-        System.out.println(teacher);
         List<College> colleges = collegeService.findAllCollege();
         ModelAndView mv = new ModelAndView();
         mv.setViewName("/teacher/update-teacher");
@@ -125,10 +128,11 @@ public class TeacherController {
     // 更新用户
     @PostMapping("/updateTeacher")
     @ResponseBody
-    public Result<Teacher> updateTeacher(@RequestParam("photo") MultipartFile file, HttpServletRequest request){
+    public Result<Teacher> updateTeacher(HttpServletRequest request){
         Result<Teacher> result = new Result<>();
         String photoUrl = null;
-        if (!file.isEmpty()) {
+        MultipartFile file = ((MultipartRequest) request).getFile("photo");
+        if (file != null) {
             String fileName = file.getOriginalFilename();  // 文件名
             assert fileName != null;
             String suffixName = fileName.substring(fileName.lastIndexOf("."));  // 后缀名
@@ -158,6 +162,10 @@ public class TeacherController {
         int pageNum = Integer.parseInt(request.getParameter("pageNum"));
 
         Teacher teacher = new Teacher(teacherId, name, age, gender, address, email, phoneNumber, collegeId, photoUrl);
+
+        if (photoUrl == null){
+            photoUrl = teacherService.findTeacherByTeacherId(teacher).getPhotoUrl();
+        }
 
         if(teacherService.updateTeacher(teacher) == 1){
             result.setMessage("更新用户成功");
@@ -190,5 +198,117 @@ public class TeacherController {
         model.addAttribute("teacher", teacher);
         model.addAttribute("courseNum", teacherService.getCourseNumByTeacherId(teacherId));
         return "/teacher/profile-teacher";
+    }
+
+    /**
+     *
+     * 老师操作部分
+     *
+     * **/
+    // 所教课程
+    @GetMapping("/course")
+     public String findAllTaughtCourse(HttpServletRequest request, Model model){
+
+        return "/teacher/course";
+    }
+    // 跳转到更新老师个人信息的界面
+    @GetMapping("/toUpdateInfo")
+    public String toUpdateInfo(Model model, HttpSession session){
+        User user = (User) session.getAttribute("user");
+        if(user == null){
+            return "redirect:/user/";
+        }else{
+            if (user.getType() == 3){
+                Teacher teacher = teacherService.findTeacherByTeacherId(new Teacher(user.getAccount()));
+                model.addAttribute("teacher", teacher);
+                return "/teacher/update-personal-info";
+            }else{
+                return "/error/404";
+            }
+        }
+    }
+
+    // 更新用户
+    @PostMapping("/updateInfo")
+    @ResponseBody
+    public Result<Teacher> toUpdateInfo(HttpServletRequest request){
+        Result<Teacher> result = new Result<>();
+        String photoUrl = null;
+        MultipartFile file = ((MultipartRequest) request).getFile("photo");
+        if (file != null) {
+            String fileName = file.getOriginalFilename();  // 文件名
+            assert fileName != null;
+            String suffixName = fileName.substring(fileName.lastIndexOf("."));  // 后缀名
+            String filePath = "F:/IDEA_projects/practice/src/main/resources/static/photo/"; // 上传后的路径
+            fileName = UUID.randomUUID() + suffixName; // 新文件名
+            File dest = new File(filePath + fileName);
+            if (!dest.getParentFile().exists()) {
+                dest.getParentFile().mkdirs();
+            }
+            try {
+                file.transferTo(dest);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            photoUrl = "/photo/" + fileName;
+        }
+
+        // 参数
+        String name = request.getParameter("name");
+        String teacherId = request.getParameter("teacherId");
+        int age = Integer.parseInt(request.getParameter("age"));
+        int gender = Integer.parseInt(request.getParameter("gender"));
+        String address = request.getParameter("address");
+        String email = request.getParameter("email");
+        String phoneNumber = request.getParameter("phoneNumber");
+        String desc = request.getParameter("desc");
+        String education = request.getParameter("education");
+
+        Teacher teacher = new Teacher(teacherId, name, age, gender, address, email, phoneNumber, photoUrl, education, desc);
+        if (photoUrl == null){
+            photoUrl = teacherService.findTeacherByTeacherId(teacher).getPhotoUrl();
+        }
+        if(teacherService.updateTeacher(teacher) == 1){
+            result.setMessage("更新用户成功");
+        }else{
+            result.setMessage("更新用户失败");
+        }
+        result.setData(teacher);
+        return result;
+    }
+
+    // 课程下的学生
+    @GetMapping("/student")
+    public String findAllStudentInCourse(HttpServletRequest request, Model model){
+
+        return "/teacher/list-student";
+    }
+
+    @GetMapping("/score")
+    public String toUploadScore(){
+        return "/teacher/upload-score";
+    }
+
+    // 上传成绩
+    @PostMapping("/uploadScore")
+    @ResponseBody
+    public Result uploadScore(HttpServletRequest request){
+        String[] studentIds = request.getParameter("studentIds").split(",");
+        String[] studentScores = request.getParameter("scores").split(",");
+        List<Float> scores = new ArrayList<>();
+        for (String score: studentScores) {
+            scores.add(Float.parseFloat(score));
+        }
+
+        System.out.println(Arrays.toString(studentIds));
+        System.out.println(Arrays.toString(studentScores));
+        Result result = new Result();
+        result.setMessage("成绩上传成功");
+        return result;
+    }
+
+    @GetMapping("/chooseCourse")
+    public String chooseCourse(){
+        return "/teacher/choose-course";
     }
 }
