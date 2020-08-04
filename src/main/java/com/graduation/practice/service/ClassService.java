94c1897f3178e832d2f4efea175ebc030d9041bf
caiyongqi <1592341907@qq.com>
2020-08-04 13:41:18 +0800

    '班级管理完善'

--- src/main/java/com/graduation/practice/service/ClassService.java
+++ src/main/java/com/graduation/practice/service/ClassService.java
@@ -2,6 +2,7 @@ package com.graduation.practice.service;
 
 import com.graduation.practice.entity.ClassInfo;
 import com.graduation.practice.entity.Classes;
+import com.graduation.practice.entity.Discipline;
 
 import java.util.List;
 
@@ -16,4 +17,7 @@ public interface ClassService {
     int addClassInfo(Classes classes);
 
     int updateClassInfo(Classes classes);
+
+    List<ClassInfo> searchClassByName(String classKeyword);
+    List<ClassInfo> findAllClassInfoByDiscipline(Discipline discipline);
 }
